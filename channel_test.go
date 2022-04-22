aae7deca82fee6e809e5c5390c6f922fb9803867
Hafiz Ramadhan <hfzrmd@gmail.com>
2022-04-22 15:48:51 +0700

    add: channel: OnlyIn, OnlyOut, TestInOutChannel

--- channel_test.go
+++ channel_test.go
@@ -31,9 +31,29 @@ func TestAsParameter(t *testing.T) {
 	defer close(channel)
 
 	go GiveMeResponse(channel)
-	
+
 	data := <- channel
 	fmt.Println(data)
 	
+	time.Sleep(5 * time.Second)
+}
+
+func OnlyIn(channel chan <- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Hafiz Ramadhan"
+}
+
+func OnlyOut(channel <- chan string) {
+	data := <- channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
 	time.Sleep(5 * time.Second)
 }
