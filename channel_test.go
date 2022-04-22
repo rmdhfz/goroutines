ed822426916894f66478a36bdf37f48079550bd6
Hafiz Ramadhan <hfzrmd@gmail.com>
2022-04-22 19:50:39 +0700

    add: channel: TestSelectChannel

--- channel_test.go
+++ channel_test.go
@@ -84,6 +84,30 @@ func TestRangeChannel(t *testing.T) {
 	for data := range channel {
 		fmt.Println("Menerima data: ",data)
 	}
-
 	fmt.Println("Selesai")
+}
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari channel2", data)
+			counter++
+		}
+		if counter == 2 {
+			break
+		}
+	}
 }
