6dc4fe2b7bcb3667c59fb43258439628cd1612e1
Hafiz Ramadhan <hfzrmd@gmail.com>
2022-04-22 19:05:13 +0700

    add: channel: TestRangeChannel

--- channel_test.go
+++ channel_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"fmt"
 	"time"
+	"strconv"
 )
 
 func TestCreateChannel(t *testing.T) {
@@ -64,8 +65,25 @@ func TestBufferedChannel(t *testing.T) {
 
 	channel <- "Hafiz"
 	channel <- "Ramadhan"
-	
+
 	fmt.Println("Selesai")
 	fmt.Println(len(channel))
 	fmt.Println(cap(channel))
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func(){
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima data: ",data)
+	}
+
+	fmt.Println("Selesai")
 }
