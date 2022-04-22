a3c14f2d88351f61b651e41bbb0315c5317319f5
Hafiz Ramadhan <hfzrmd@gmail.com>
2022-04-22 15:38:01 +0700

    add: channel: TestAsParameter

--- channel_test.go
+++ channel_test.go
@@ -20,3 +20,20 @@ func TestCreateChannel(t *testing.T) {
 	
 	time.Sleep(5 * time.Second)
 }
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Hafiz Ramadhan"
+}
+
+func TestAsParameter(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+	
+	data := <- channel
+	fmt.Println(data)
+	
+	time.Sleep(5 * time.Second)
+}
