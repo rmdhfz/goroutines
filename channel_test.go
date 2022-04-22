eb30ba5aa8c8f0917ec8f03e6a16f6b5f5ef5053
Hafiz Ramadhan <hfzrmd@gmail.com>
2022-04-22 19:54:02 +0700

    add: channel: TestDefaultSelectChannel

--- channel_test.go
+++ channel_test.go
@@ -111,3 +111,30 @@ func TestSelectChannel(t *testing.T) {
 		}
 	}
 }
+
+func TestDefaultSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari channel2", data)
+			counter++
+		default:
+			fmt.Println("Menunggu data")
+		}
+		if counter == 2 {
+			break
+		}
+	}
+}
