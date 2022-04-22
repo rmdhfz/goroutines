43dc911c4b9ace2c02d09118365f4faa24450b2c
Hafiz Ramadhan <hfzrmd@gmail.com>
2022-04-22 20:28:37 +0700

    add: mutex: RWMutex

--- mutex_test.go
+++ mutex_test.go
@@ -24,3 +24,36 @@ func TestMutex(t *testing.T) {
 	time.Sleep(5 * time.Second)
 	fmt.Println("Counter = ", x)
 }
+
+type BankAccount struct {
+	RWMutex sync.RWMutex
+	Balance int
+}
+
+func (account *BankAccount) AddBalance(amount int) {
+	account.RWMutex.Lock()
+	account.Balance = account.Balance + amount
+	account.RWMutex.Unlock()
+}
+
+func (account *BankAccount) GetBalance() int {
+	account.RWMutex.RLock()
+	balance := account.Balance
+	account.RWMutex.RUnlock()
+	return balance
+}
+
+func TestRWMutex(t *testing.T) {
+	account := BankAccount{}
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				account.AddBalance(1)
+				fmt.Println(account.GetBalance())
+			}
+		}()
+	}
+	time.Sleep(5 * time.Second)
+	fmt.Println("Total Balance", account.GetBalance())
+}
