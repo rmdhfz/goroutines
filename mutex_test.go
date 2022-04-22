714096ba642f729380b588cd67263720219645ac
Hafiz Ramadhan <hfzrmd@gmail.com>
2022-04-22 20:14:28 +0700

    add: mutex_test

---
+++ mutex_test.go
@@ -0,0 +1,26 @@
+package belajar_golang_goroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestMutex(t *testing.T) {
+	x := 0
+	var mutex sync.Mutex
+
+	for i := 1; i <= 1000; i++ {
+		go func() {
+			for j := 1; j <= 100; j++ {
+				mutex.Lock()
+				x = x + 1
+				mutex.Unlock()
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Counter = ", x)
+}
