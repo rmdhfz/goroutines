2655138f90da6900e66b5a70743eafe433e932e0
Hafiz Ramadhan <hfzrmd@gmail.com>
2022-04-22 15:33:32 +0700

    add: channel

---
+++ channel_test.go
@@ -0,0 +1,22 @@
+package belajar_golang_goroutines
+
+import (
+	"testing"
+	"fmt"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go func(){
+		time.Sleep(2 * time.Second)
+		channel <- "Hafiz Ramadhan"
+		fmt.Println("Selesai mengirim data ke channel")
+	}()
+	data := <- channel
+	fmt.Println(data)
+	
+	time.Sleep(5 * time.Second)
+}
