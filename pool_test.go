3771a4bdcdd7eda96f428ecb8e44fa27daadad41
Hafiz Ramadhan <hfzrmd@gmail.com>
2022-04-23 07:41:17 +0700

    add: pool

---
+++ pool_test.go
@@ -0,0 +1,32 @@
+package belajar_golang_goroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestPool(t *testing.T) {
+	pool := sync.Pool{
+		New: func() interface{} {
+			return "New"
+		},
+	}
+
+	pool.Put("Hafiz")
+	pool.Put("Ramadhan")
+	pool.Put("Budiman")
+
+	for i := 1; i < 10; i++ {
+		go func() {
+			data := pool.Get()
+			fmt.Println(data)
+			time.Sleep(1 * time.Second)
+			pool.Put(data)
+		}()
+	}
+
+	time.Sleep(11 * time.Second)
+	fmt.Println("Selesai")
+}
